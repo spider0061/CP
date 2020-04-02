2ea8104271cd8842cadd8e722442b49b284d0c30
Ashutosh Sharma <ashutoshsharma0061@gmail.com>
2020-04-02 18:29:49 +0530

    CP template added.

---
+++ compi_template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define show(x) cout<< #x <<" is "<< x <<"\n"
+#define pb(t) push_back(t)
+#define len(t) t.size())
+#define all(t) t.begin(),t.end()
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef vector<PII> VPII;
+
+int main() {
+	#ifndef ONLINE_JUDGE
+        //freopen("input.txt", "rt", stdin);
+        //freopen("output.txt", "wt", stdout);
+    #endif
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    	
+	return 0;
+}
